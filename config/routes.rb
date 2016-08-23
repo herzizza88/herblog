27688e140689324c403a6284d36a4b510730aad3
azizahsiddique <azizah_siddique@nlb.gov.sg>
2016-08-23 08:32:54 +0000

    load page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   end
 
   root "posts#index"
-
+get '/', to: 'posts#index'
+  get '/users/sign_in', to: 'devise/sessions#new' 'devise/sessions#create'
   get '/about', to: 'pages#about'
+  get '/users/sign_out', to: 'devise/sessions#destroy'
 
 end
